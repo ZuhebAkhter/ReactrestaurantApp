d4b1ec32d76d39d33a8c286b518085ce8b1611c8
ZuhebAkhter <zuheb1998@gmail.com>
2023-07-20 12:17:21 +0530

    addingsummary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Component/Layout/Header";
+import Summary from "./Component/Meals/Summary";
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <Summary/>
     </React.Fragment>
     
   );
